64a4f81c21da50e7ba753aa4f20907a03d5320ef
mayaishere2 <111202130+mayaishere2@users.noreply.github.com>
2024-01-14 15:28:24 +0100

    Delete javascript directory

--- javascript/text-limit.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var descriptions = document.querySelectorAll(".product .product_description");
-
-  descriptions.forEach(function (description) {
-    description.classList.add("truncate");
-  });
-});
